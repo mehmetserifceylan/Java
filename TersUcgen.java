e96619f489954fce1cf68201c8f1fd499abdfbf4
mehmetserifceylan <65469373+mehmetserifceylan@users.noreply.github.com>
2023-01-18 18:47:53 +0300

    Add files via upload

---
+++ TersUcgen.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz:");
+        int s=input.nextInt();
+
+        for (int i = 0; i <s ; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 2*s-1; k >=(2*i+1) ; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
