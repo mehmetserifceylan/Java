fe73cecd3dd13f18165d237ca1839d63273d946b
ask0 <voidask0@gmail.com>
2023-01-20 01:16:45 +0300

    Update TersUcgen.java
    Fixed extra blank from the first line

--- TersUcgen.java
+++ TersUcgen.java
@@ -7,7 +7,7 @@ public class TersUcgen {
         int s=input.nextInt();
 
         for (int i = 0; i <s ; i++) {
-            for (int j = 0; j <= i; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
             }
             for (int k = 2*s-1; k >=(2*i+1) ; k--) {
